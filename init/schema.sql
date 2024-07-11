f9b31dd5f467c77e50fec9ece8ce4704f8844d6a
Isaac Diniz <isacpdalves@gmail.com>
2024-07-11 18:46:55 -0300

    feat: Implementing link finder

--- init/schema.sql
+++ init/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS 'trips'
     end_date DATETIME,
     owner_name TEXT NOT NULL,
     owner_email TEXT NOT NULL,
-    status INTEGER -- 1 para TRUE, 0 para FALSO
+    status INTEGER -- 1 para Tue, 0 para False
 );
 
 CREATE TABLE IF NOT EXISTS 'emails_to_invite'
@@ -23,5 +23,25 @@ CREATE TABLE IF NOT EXISTS 'links'
     trip_id TEXT,
     link TEXT NOT NULL,
     title TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
+
+CREATE TABLE IF NOT EXISTS 'participants'
+(
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    emails_to_invite_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    is_confirmed INTEGER, -- 1 para True, 0 para False
+    FOREIGN KEY (trip_id) REFERENCES trips(id),
+    FOREIGN KEY (emails_to_invite_id) REFERENCES emails_to_invite(id)
+);
+
+CREATE TABLE IF NOT EXISTS 'activities'
+(
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    occurs_at DATETIME,
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
